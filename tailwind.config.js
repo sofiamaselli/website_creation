0d29173159b61292ecebd1affd2dd1d107583656
Sofia Maselli <81928936+sofiamaselli@users.noreply.github.com>
2024-02-24 22:55:20 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.html', './src/**/*.svelte', './src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
