b101f65326f55c88b45400e8fec601cad70218eb
Sofia Maselli <81928936+sofiamaselli@users.noreply.github.com>
2024-03-08 13:48:47 +0100

    Create Contact_Info.svelte

---
+++ src/routes/components/Contact_Info.svelte
@@ -0,0 +1,11 @@
+<div class="bg-blue-400 p-4 mb-6 rounded">
+  <div class="mb-8">
+    <h2 class="text-xl font-bold mb-2">Contact Information</h2>
+    <hr class="border-b-2 border-blue-300 mb-4">
+    <p class="font-bold">Email</p>
+    <p>sofia.maselli@essec.edu</p>
+    <p class="font-bold">Phone</p>
+    <p>(+39) 3665547025</p>
+    <a href="https://www.linkedin.com/in/sofia-maselli-38031a1b8/" class="text-blue-600 underline">LinkedIn</a>
+  </div>
+</div>
