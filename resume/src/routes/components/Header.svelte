d416fd0d58bf941d0ed4cc18cc1d791604755abb
Sofia Maselli <81928936+sofiamaselli@users.noreply.github.com>
2024-03-08 14:26:19 +0100

    Add files via upload

---
+++ resume/src/routes/components/Header.svelte
@@ -0,0 +1,12 @@
+<div>
+    <div class="fixed left-0 top-0 w-full h-8 bg-gradient-to-r from-blue-400 to-green-400"></div>
+    <div class="container mx-auto p-6">
+    <header class="relative text-5xl font-bold text-center py-8">Sofia Maselli</header>
+    <p class="text-center text-xl pb-6">
+        <span class="font-bold">Email:</span> sofia.maselli@esssec.edu &nbsp;&nbsp;&nbsp;
+        <span class="font-bold">Phone:</span> (+39) 3665547025 &nbsp;&nbsp;&nbsp;
+        <a href="https://www.linkedin.com/in/sofia-maselli-38031a1b8/" class="text-blue-600 underline">LinkedIn</a>
+    </p>
+
+    </div>
+</div>
