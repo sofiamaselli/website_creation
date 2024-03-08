2c4269f5055c3334633176b28f84f4e39712561c
Sofia Maselli <81928936+sofiamaselli@users.noreply.github.com>
2024-03-08 13:26:08 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.html', './src/**/*.svelte', './src/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
