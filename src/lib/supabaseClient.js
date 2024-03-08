38b5268cb2fa06c3a257a14c41376d4971e34c92
Sofia Maselli <81928936+sofiamaselli@users.noreply.github.com>
2024-03-08 14:27:10 +0100

    Delete src directory

--- src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://rcexvelvrergeemyjivu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjZXh2ZWx2cmVyZ2VlbXlqaXZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk1NDkyMTAsImV4cCI6MjAyNTEyNTIxMH0.D7FJUytHCuLI7pbv-pVkWWHS5ZmPT0NvTF6s_jqw-L0')
